2331513d98027ef1cbdead51c3bf4204f6d8b99a
Soura700 <108584756+Soura700@users.noreply.github.com>
2023-08-26 00:16:14 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,19 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const taskRoute = require("./routers/task")
+const cors = require("cors");
+
 
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 
 // Step 1:
 dotenv.config();
